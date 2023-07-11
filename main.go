40ba63f86d0b888d5b6edbf30041e5780e075494
Frederik Schmitt <f.schmitt@shopware.com>
2023-07-11 14:06:13 +0200

    Add db migrations

--- main.go
+++ main.go
@@ -3,10 +3,28 @@ package main
 import (
 	"github.com/fschmtt/thw-leaderboard/api"
 	"github.com/fschmtt/thw-leaderboard/db"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func main() {
 	d, err := db.Connect()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	driver, err := mysql.WithInstance(d, &mysql.Config{})
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"mysql",
+		driver,
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = m.Up()
 	if err != nil {
 		panic(err.Error())
 	}
--- migrations/1689036093_create_competitor_table.up.sql
+++ migrations/1689036093_create_competitor_table.up.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS `competitor`
     `offset_x`     int(11)      NOT NULL,
     `offset_y`     int(11)      NOT NULL,
     `fault_points` int(11)      NOT NULL,
-    `created_at`   datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP
+    `created_at`   datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
